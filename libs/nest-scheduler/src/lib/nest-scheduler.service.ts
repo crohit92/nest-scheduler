dd4b7ad8b4f5146d76cc760218d62826c39270b5
Rohit Chopra <rohit.chopra01@nagarro.com>
2022-06-30 15:00:04 +0530

    WIP patching the schedule method to log all the output to a file

--- libs/nest-scheduler/src/lib/nest-scheduler.service.ts
+++ libs/nest-scheduler/src/lib/nest-scheduler.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ScheduledJob } from './models/scheduled-job';
 import { JobStorageService } from './storage/job-storage.service';
-import { schedule, getTasks, ScheduledTask } from "node-cron";
+import { schedule as libSchedule, getTasks, ScheduledTask, ScheduleOptions } from "node-cron";
+
+function schedule(job: ScheduledJob, func: (now: Date) => void, options?: ScheduleOptions) {
+  return libSchedule(job.cron, function (now) {
+    console.log(`Execution of ${job.name} started @ ${getTimeString()} `);
+    try {
+      // Patch console.log and redirect output to a file
+      func(now);
+      // Restore console.log
+    } catch (error) {
+      // set status as error
+      console.log(`Error occured in ${job.name} @ ${getTimeString()} `);
+    }
+    // set status as executed
+    console.log(`Execution of ${job.name} ended @ ${getTimeString()} `);
+  }, options);
+
+  function getTimeString() {
+    const d = new Date();
+    return `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${d.getDate().toString().padStart(2, '0')} ${d.getHours().toString().padStart(2, '0')}:${d.getMinutes().toString().padStart(2, '0')}:${d.getSeconds().toString().padStart(2, '0')}`
+  }
+}
 @Injectable()
 export class JobSchedulerService {
   jobNames = new Set<string>();
@@ -16,7 +37,7 @@ export class JobSchedulerService {
     // If this job does not exist in store
     if (existingJobIndex === -1) {
       // Schedule it
-      const task = schedule(job.cron, handler);
+      const task = schedule(job, handler);
       // and store it
       job.id = (task as any).options.name;
       allJobs.push(new ScheduledJob(job));
@@ -33,14 +54,14 @@ export class JobSchedulerService {
       job.enabled = existingJob.enabled;
       if (existingJob.enabled) {
         // schedule this task --
-        task = schedule(job.cron, handler);
+        task = schedule(job, handler);
         if (runImmidiately) {
           handler(new Date());
         }
       } else {
         // else schedule the task and pause it immidiately
         // so it can be started via the api.
-        task = schedule(job.cron, handler);
+        task = schedule(job, handler);
         task.stop()
       }
       // update the task id in store, since on scheduling
